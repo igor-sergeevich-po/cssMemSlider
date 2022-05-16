d950389766d6f5b9fdc4ab853420816e9b8be438
Igor_PO <89515076779@ya.ru>
2022-05-16 17:56:40 +0300

    feat: add animation slider

---
+++ src/index.js
@@ -0,0 +1,51 @@
+function moveSlides(){
+    let memTexts = ['День получки','skills you are proud of',"because at heart I'm cat", 'when you got a pet', 'Russian IT development']
+const pictureItem = document.querySelector(".picture"),
+    imageText = document.querySelector(".image-text"),
+    window = document.querySelector('.window-open'),
+    navigation = document.querySelector(".navigation-points");
+    let navItems = document.querySelectorAll(".custom-nav-item")
+let position = 0;
+
+navigation.addEventListener('click', (e)=> {
+    
+    console.log(pictureItem.getBoundingClientRect().width)
+  
+    navItems.forEach(item => {
+        item.classList.add("slidein")
+        item.classList.remove('checked')
+    })
+    e.target.classList.add('checked')
+
+
+    if(e.target.id == 'nav-0'){
+        position = 0;
+        imageText.textContent = `${memTexts[0]}`
+        window.style = `transform:translateX(${position}px)`
+
+    }else if(e.target.id == 'nav-1'){
+        position =-`${(pictureItem.getBoundingClientRect().width)}`;
+        imageText.textContent = `${memTexts[1]}`
+        window.style = `transform:translateX(${position}px)`
+
+    }else if(e.target.id == 'nav-2'){
+        position =-`${(pictureItem.getBoundingClientRect().width)*2}`;
+        imageText.textContent = `${memTexts[2]}`
+        window.style = `transform:translateX(${position}px)`
+
+    }else if(e.target.id == 'nav-3'){
+        position =-`${(pictureItem.getBoundingClientRect().width)*3}`;
+        imageText.textContent = `${memTexts[3]}`
+        window.style = `transform:translateX(${position}px)`
+
+    }else if(e.target.id == 'nav-4'){
+        position =-`${(pictureItem.getBoundingClientRect().width)*4}`;
+        imageText.textContent = `${memTexts[4]}`
+        window.style = `transform:translateX(${position}px)`
+    }
+    navigation.classList.remove('checked')
+
+    imageText.classList.toggle('animation-text')
+})
+}
+moveSlides()
